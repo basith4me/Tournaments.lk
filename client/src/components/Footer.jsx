45b63400ca0a42d451c1fbdc4ace0adef243c5f6
Abdul Basith <116253542+basith4me@users.noreply.github.com>
2025-11-07 22:37:28 +0530

    fix: adjust footer padding and height for better layout

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="fixed bottom-0 left-0 w-full p-">
-      <div className="bg-white/30 backdrop-blur-md border border-white/20 rounded-lg shadow-lg p-6 text-center">
+    <footer className="fixed bottom-0 left-0 w-full h-">
+      <div className="bg-white/30 backdrop-blur-md border border-white/20 rounded-lg shadow-lg p- text-center">
         <p className="text-black text-xl font-medium">
           © {new Date().getFullYear()} All rights reserved to TournamentsLK
         </p>
